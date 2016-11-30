189bda7fdcd8790464f6ffcc0e35ced05b41505a
Derek Richard <drichard@pivotal.io>
2016-11-29 17:26:49 -0800

    Work on deploy-director task
    - Start using default environment of ol-smokey
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- tasks/deploy-director/task.sh
+++ tasks/deploy-director/task.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-exit 1
+bosh create-env bosh-deployment/bosh.yml \
+  --state bosh-state/bosh-state.json \
+  --ops-file bosh-deployment/gcp/cpi.yml \
+  --ops-file bosh-deployment/uaa.yml \
+  --vars-store bosh-creds/bosh-creds.yml \
+  --vars-file bosh-vars/bosh-vars.yml
+#  --ops-file credhub/credhub.yml \
