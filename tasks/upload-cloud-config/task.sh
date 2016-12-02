da3f0efa5956ff519daf61ea80ee1f3c040f3183
Dan Wendorf <dwendorf@pivotal.io>
2016-12-02 15:50:06 -0800

    Add upload-cloud-config task
    Add helper script to setup a director environment
    
    [#135298675]

--- tasks/upload-cloud-config/task.sh
+++ tasks/upload-cloud-config/task.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-exit 1
+source pcf-bosh-ci/scripts/load-director-environment.sh bosh-creds/bosh-creds.yml terraform-state/metadata
+
+bosh update-cloud-config pcf-bosh-ci/cloud-config.yml
