5fe9992d763e3242cfd491f0d5ed067df78eac4f
Derek Richard <drichard@pivotal.io>
2016-12-02 10:43:39 -0800

    Set external_ip variable from Terraform state
    [#135152997]
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- tasks/deploy-director/task.sh
+++ tasks/deploy-director/task.sh
@@ -10,4 +10,5 @@ bosh create-env bosh-deployment/bosh.yml \
   --ops-file pcf-bosh-ci/ops-files/director-overrides.yml \
   --vars-store bosh-creds/bosh-creds.yml \
   --vars-file bosh-vars/bosh-vars.yml \
-  --var file_path_to_credhub_release="file://$PWD/credhub-release/credhub-0.3.0.tgz"
+  --var file_path_to_credhub_release="file://$PWD/credhub-release/credhub-0.3.0.tgz" \
+  --var external_ip="$(jq -r .director_external_ip terraform-state/metadata)"
