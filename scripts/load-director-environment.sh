abd596acc186797204a875b19879c5f5a978ff36
Dan Wendorf <dwendorf@pivotal.io>
2016-12-02 15:59:18 -0800

    Use correct BOSH_ENVIRONMENT variable
    Clean up useless echo
    
    [#135298675]

--- scripts/load-director-environment.sh
+++ scripts/load-director-environment.sh
@@ -3,11 +3,9 @@
 creds_path="$1"
 terraform_state_path="$2"
 
-echo "puts JSON.pretty_generate(YAML.load($creds_path))"
-
 creds_json=$(ruby -ryaml -rjson -e "puts JSON.pretty_generate(YAML.load_file('$creds_path'))")
 
 export BOSH_USER="admin"
 export BOSH_PASSWORD=$(echo "$creds_json" | jq -r .admin_password)
-export BOSH_TARGET=$(jq -r .director_external_ip "$terraform_state_path")
+export BOSH_ENVIRONMENT=$(jq -r .director_external_ip "$terraform_state_path")
 export BOSH_CA_CERT=$(echo "$creds_json" | jq -r .director_ssl.ca)
