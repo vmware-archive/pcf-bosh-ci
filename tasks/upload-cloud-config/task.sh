666c5558448e380c6154eed7c4b05a6e9a95618e
Dan Wendorf <dwendorf@pivotal.io>
2016-12-02 16:01:34 -0800

    Do not wait for a human to say yes when boshing
    [#135298675]

--- tasks/upload-cloud-config/task.sh
+++ tasks/upload-cloud-config/task.sh
@@ -2,4 +2,4 @@
 
 source pcf-bosh-ci/scripts/load-director-environment.sh bosh-creds/bosh-creds.yml terraform-state/metadata
 
-bosh update-cloud-config pcf-bosh-ci/cloud-config.yml
+bosh -n update-cloud-config pcf-bosh-ci/cloud-config.yml
