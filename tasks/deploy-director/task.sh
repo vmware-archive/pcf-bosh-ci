ba74f7044a9b20e23111c5bcf9938991fb259442
Dan Wendorf <dwendorf@pivotal.io>
2016-12-05 12:27:39 -0800

    Ensure we always update bosh state and creds even in the face of failure
    [#135535505]
    
    Signed-off-by: Derek Richard <drichard@pivotal.io>

--- tasks/deploy-director/task.sh
+++ tasks/deploy-director/task.sh
@@ -2,18 +2,18 @@
 
 set -e
 
+cp bosh-state/bosh-state.json new-bosh-state/bosh-state.json
+cp bosh-creds/bosh-creds.yml new-bosh-creds/bosh-creds.yml
+
 bosh create-env bosh-deployment/bosh.yml \
-  --state bosh-state/bosh-state.json \
+  --state new-bosh-state/bosh-state.json \
   --ops-file pcf-bosh-ci/ops-files/ljfranklin_external-ip-not-recommended.yml \
   --ops-file bosh-deployment/gcp/cpi.yml \
   --ops-file bosh-deployment/uaa.yml \
   --ops-file pcf-bosh-ci/ops-files/uaa-with-external-ip.yml \
   --ops-file pcf-bosh-ci/ops-files/credhub.yml \
   --ops-file pcf-bosh-ci/ops-files/director-overrides.yml \
-  --vars-store bosh-creds/bosh-creds.yml \
+  --vars-store new-bosh-creds/bosh-creds.yml \
   --vars-file bosh-vars/bosh-vars.yml \
   --var file_path_to_credhub_release="file://$PWD/credhub-release/credhub-0.3.0.tgz" \
   --var external_ip="$(jq -r .director_external_ip terraform-state/metadata)"
-
-cp bosh-state/bosh-state.json new-bosh-state/bosh-state.json
-cp bosh-creds/bosh-creds.yml new-bosh-creds/bosh-creds.yml
