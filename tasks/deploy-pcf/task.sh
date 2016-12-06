9d61b53e478042398937771c1587a60945ad7e92
Derek Richard <drichard@pivotal.io>
2016-12-06 12:02:21 -0800

    Use var-store for deploy-pcf cred management
    - Credhub and the director are currently not talking to each other in such a way that we can rely entirely on them for credential generation and storage
    
    [#135153103]

--- tasks/deploy-pcf/task.sh
+++ tasks/deploy-pcf/task.sh
@@ -2,7 +2,10 @@
 
 source pcf-bosh-ci/scripts/load-director-environment.sh bosh-creds/bosh-creds.yml
 
+cp cf-creds/cf-creds.yml new-cf-creds/cf-creds.yml
+
 bosh -n deploy cf-deployment/cf-deployment.yml \
   --deployment cf \
   --ops-file cf-deployment/opsfiles/gcp.yml \
+  --var-store new-cf-creds/cf-creds.yml \
   --var system_domain=ol-smokey.gcp.pcf-bosh.cf-app.com
