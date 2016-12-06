49795f686fd86be98e86b603af31a83f9fba536b
Derek Richard <drichard@pivotal.io>
2016-12-06 10:10:15 -0800

    Update pcf-deploy to use cf-deployment for deploying cloudfoundry
    - Hardcode to ol-smokey env
    - Track master of cf-deployment
    
    [#135153103]

--- tasks/deploy-pcf/task.sh
+++ tasks/deploy-pcf/task.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-exit 1
+source pcf-bosh-ci/scripts/load-director-environment.sh bosh-creds/bosh-creds.yml
+
+bosh -n deploy cf-deployment/cf-deployment.yml \
+  --deployment cf \
+  --ops-file cf-deployment/opsfiles/gcp.yml \
+  --var system_domain=ol-smokey.gcp.pcf-bosh.cf-app.com
