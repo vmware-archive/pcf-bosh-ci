a3d8c89232d45d88594b76511428e452d7518bcc
Derek Richard <drichard@pivotal.io>
2016-12-05 16:37:40 -0800

    Update director-environment script to use client auth
    [#135298675]

--- scripts/load-director-environment.sh
+++ scripts/load-director-environment.sh
@@ -3,7 +3,8 @@
 creds_path="$1"
 creds_json=$(ruby -ryaml -rjson -e "puts JSON.pretty_generate(YAML.load_file('$creds_path'))")
 
-export BOSH_USER="admin"
-export BOSH_PASSWORD=$(echo "$creds_json" | jq -r .admin_password)
+export BOSH_CLIENT="ci"
+export BOSH_CLIENT_SECRET=$(echo "$creds_json" | jq -r .ci_secret)
+
 export BOSH_ENVIRONMENT="director.ol-smokey.gcp.pcf-bosh.cf-app.com"
 export BOSH_CA_CERT=$(echo "$creds_json" | jq -r .director_ssl.ca)
--- tasks/upload-cloud-config/task.sh
+++ tasks/upload-cloud-config/task.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-source pcf-bosh-ci/scripts/load-director-environment.sh bosh-creds/bosh-creds.yml terraform-state/metadata
+source pcf-bosh-ci/scripts/load-director-environment.sh bosh-creds/bosh-creds.yml
 
 bosh -n update-cloud-config pcf-bosh-ci/cloud-config.yml
