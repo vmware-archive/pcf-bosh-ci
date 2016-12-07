830f8656ab22f942ad4f4e79230e65304ff6844f
Dan Wendorf <dwendorf@pivotal.io>
2016-12-06 17:33:24 -0800

    Set apps domain to be different than sys domain
    [#135153103]

--- tasks/deploy-pcf/task.sh
+++ tasks/deploy-pcf/task.sh
@@ -7,5 +7,6 @@ cp cf-creds/cf-creds.yml new-cf-creds/cf-creds.yml
 bosh -n deploy cf-deployment/cf-deployment.yml \
   --deployment cf \
   --ops-file cf-deployment/opsfiles/gcp.yml \
+  --ops-file ops-files/cf-apps-domain.yml \
   --vars-store new-cf-creds/cf-creds.yml \
   --var system_domain=sys.ol-smokey.gcp.pcf-bosh.cf-app.com
