bee4855c988f46e601538d109b8c3458d9b35939
Derek Richard <drichard@pivotal.io>
2016-12-06 16:31:14 -0800

    Change system domain to include sys. part
    [#135153103]
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- tasks/deploy-pcf/task.sh
+++ tasks/deploy-pcf/task.sh
@@ -8,4 +8,4 @@ bosh -n deploy cf-deployment/cf-deployment.yml \
   --deployment cf \
   --ops-file cf-deployment/opsfiles/gcp.yml \
   --vars-store new-cf-creds/cf-creds.yml \
-  --var system_domain=ol-smokey.gcp.pcf-bosh.cf-app.com
+  --var system_domain=sys.ol-smokey.gcp.pcf-bosh.cf-app.com
