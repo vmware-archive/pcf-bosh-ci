5791b42b09d58f8526de1e06ea32d9aa8a105504
Derek Richard <drichard@pivotal.io>
2016-12-07 14:20:24 -0800

    Use passwords for passwords instead of targets for passwords
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- tasks/enable-diego-docker/task.sh
+++ tasks/enable-diego-docker/task.sh
@@ -8,6 +8,6 @@ cf_username="admin"
 cf_password=$(echo "$creds_json" | jq -r .uaa_scim_users_admin_password)
 cf_target="https://api.sys.ol-smokey.gcp.pcf-bosh.cf-app.com"
 
-cf login -a "$cf_target" -u "$cf_username" -p "$cf_target" --skip-ssl-validation
+cf login -a "$cf_target" -u "$cf_username" -p "$cf_password" --skip-ssl-validation
 
 cf enable-feature-flag diego_docker
