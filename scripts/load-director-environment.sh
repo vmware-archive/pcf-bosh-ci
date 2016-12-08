531ff765c1f803379ab3f7e74b82aaa92e07402a
Derek Richard <drichard@pivotal.io>
2016-12-07 16:56:50 -0800

    Make shellcheck happy
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- scripts/load-director-environment.sh
+++ scripts/load-director-environment.sh
@@ -4,7 +4,9 @@ creds_path="$1"
 creds_json=$(ruby -ryaml -rjson -e "puts JSON.pretty_generate(YAML.load_file('$creds_path'))")
 
 export BOSH_CLIENT="ci"
-export BOSH_CLIENT_SECRET=$(echo "$creds_json" | jq -r .ci_secret)
+export BOSH_CLIENT_SECRET
+BOSH_CLIENT_SECRET=$(echo "$creds_json" | jq -r .ci_secret)
 
 export BOSH_ENVIRONMENT="director.ol-smokey.gcp.pcf-bosh.cf-app.com"
-export BOSH_CA_CERT=$(echo "$creds_json" | jq -r .director_ssl.ca)
+export BOSH_CA_CERT
+BOSH_CA_CERT=$(echo "$creds_json" | jq -r .director_ssl.ca)
