b0bb948995f024cfa63136ad4568274028c40e6b
Dan Wendorf <dwendorf@pivotal.io>
2016-12-08 14:36:44 -0800

    Add updated gopath to path to run-rats task
    - The RATs do not add their vendored gopath to PATH, making ginkgo in particular unable to execute
    
    [#135546581]
    
    Signed-off-by: Derek Richard <drichard@pivotal.io>

--- tasks/run-rats/task.sh
+++ tasks/run-rats/task.sh
@@ -4,6 +4,7 @@ set -e
 
 export CONFIG="$PWD/cf-rats-config/cf-rats-config.json"
 export GOPATH="$PWD/routing-release"
+export PATH="${GOPATH}/bin":$PATH
 
 cd "$GOPATH/src/code.cloudfoundry.org/routing-acceptance-tests/"
 
