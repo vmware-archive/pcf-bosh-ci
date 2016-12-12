6d7d9e41e933281c2bed98945c85bcff4434a8d1
Dan Wendorf <dwendorf@pivotal.io>
2016-12-12 12:06:52 -0800

    Get release versions from version files
    [#135386141]
    
    Signed-off-by: Derek Richard <drichard@pivotal.io>

--- tasks/deploy-pcf.sh
+++ tasks/deploy-pcf.sh
@@ -11,19 +11,19 @@ bosh -n deploy cf-deployment/cf-deployment.yml \
   --vars-store new-cf-creds/cf-creds.yml \
   --var system_domain=sys.ol-smokey.gcp.pcf-bosh.cf-app.com \
   --var notifications_ui_release_path="file://$PWD/notifications-ui-release/notifications-ui*.tgz" \
-  --var notifications_ui_release_version="file://$PWD/notifications-ui-release/version" \
+  --var notifications_ui_release_version="$(cat "file://$PWD/notifications-ui-release/version")" \
   --var cf_autoscaling_release_path="file://$PWD/cf-autoscaling-release/cf-autoscaling*.tgz" \
-  --var cf_autoscaling_release_version="file://$PWD/cf-autoscaling-release/version" \
+  --var cf_autoscaling_release_version="$(cat "file://$PWD/cf-autoscaling-release/version")" \
   --var console_release_path="file://$PWD/console-release/*.tgz" \
-  --var console_release_version="file://$PWD/console-release/version" \
+  --var console_release_version="$(cat "file://$PWD/console-release/version")" \
   --var cf_release_path="file://$PWD/cf-release/*.tgz" \
   --var postgres_release_path="file://$PWD/postgres-release/*.tgz" \
-  --var cf_release_version="file://$PWD/cf-release/version" \
-  --var postgres_release_version="file://$PWD/postgres-release/version" \
+  --var cf_release_version="$(cat "file://$PWD/cf-release/version")" \
+  --var postgres_release_version="$(cat "file://$PWD/postgres-release/version")" \
   --var mysql_monitoring_release_path="file://$PWD/mysql-monitoring-release/*.tgz" \
-  --var mysql_monitoring_release_version="file://$PWD/mysql-monitoring-release/version" \
+  --var mysql_monitoring_release_version="$(cat "file://$PWD/mysql-monitoring-release/version")" \
   --var mysql_backup_release_path="file://$PWD/mysql-backup-release/*.tgz" \
-  --var mysql_backup_release_version="file://$PWD/mysql-backup-release/version"
+  --var mysql_backup_release_version="$(cat "file://$PWD/mysql-backup-release/version")" \
 
 #  --var service_backup_release_path="file://$PWD/service-backup-release/service-backup*.tgz"
 #  --var service_backup_release_version="file://$PWD/service-backup-release/version"
