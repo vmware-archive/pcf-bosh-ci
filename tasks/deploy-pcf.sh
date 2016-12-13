4b27fa8212b515e4be35e0fd28070e2a871d173e
Dan Wendorf <dwendorf@pivotal.io>
2016-12-12 16:31:08 -0800

    Include yaml string interpretation in string bash variable
    - Writing valid yaml strings in bash is a bit silly, but it is what the bosh cli needs
    
    [#135386141]
    
    Signed-off-by: Derek Richard <drichard@pivotal.io>

--- tasks/deploy-pcf.sh
+++ tasks/deploy-pcf.sh
@@ -12,18 +12,18 @@ bosh -n deploy cf-deployment/cf-deployment.yml \
   --vars-store new-cf-creds/cf-creds.yml \
   --var system_domain=sys.ol-smokey.gcp.pcf-bosh.cf-app.com \
   --var "notifications_ui_release_path=file://$(ls "$PWD"/notifications-ui-release/notifications-ui*.tgz)" \
-  --var "notifications_ui_release_version=$(cat "$PWD/notifications-ui-release/version")" \
+  --var "notifications_ui_release_version=\"$(cat "$PWD/notifications-ui-release/version")\"" \
   --var "cf_autoscaling_release_path=file://$(ls "$PWD"/cf-autoscaling-release/cf-autoscaling*.tgz)" \
-  --var "cf_autoscaling_release_version=$(cat "$PWD/cf-autoscaling-release/version")" \
+  --var "cf_autoscaling_release_version=\"$(cat "$PWD/cf-autoscaling-release/version")\"" \
   --var "console_release_path=file://$(ls "$PWD"/console-release/*.tgz)" \
-  --var "console_release_version=$(cat "$PWD/console-release/version")" \
+  --var "console_release_version=\"$(cat "$PWD/console-release/version")\"" \
   --var "cf_release_path=file://$(ls "$PWD"/cf-release/*.tgz)" \
-  --var "cf_release_version=$(cat "$PWD/cf-release/version")" \
+  --var "cf_release_version=\"$(cat "$PWD/cf-release/version")\"" \
   --var "postgres_release_path=file://$(ls "$PWD"/postgres-release/*.tgz)" \
-  --var "postgres_release_version=$(cat "$PWD/postgres-release/version")" \
+  --var "postgres_release_version=\"$(cat "$PWD/postgres-release/version")\"" \
   --var "mysql_monitoring_release_path=file://$(ls "$PWD"/mysql-monitoring-release/*.tgz)" \
-  --var "mysql_monitoring_release_version=$(cat "$PWD/mysql-monitoring-release/version")" \
+  --var "mysql_monitoring_release_version=\"$(cat "$PWD/mysql-monitoring-release/version")\"" \
   --var "mysql_backup_release_path=file://$(ls "$PWD"/mysql-backup-release/*.tgz)" \
-  --var "mysql_backup_release_version=$(cat "$PWD/mysql-backup-release/version")" \
+  --var "mysql_backup_release_version=\"$(cat "$PWD/mysql-backup-release/version")\"" \
   --var "service_backup_release_path=file://$(ls "$PWD"/service-backup-release/service-backup*.tgz)" \
-  --var "service_backup_release_version=$(jq -r .Release.Version "$PWD/service-backup-release/metadata.json")"
+  --var "service_backup_release_version=\"$(jq -r .Release.Version "$PWD/service-backup-release/metadata.json")\""
