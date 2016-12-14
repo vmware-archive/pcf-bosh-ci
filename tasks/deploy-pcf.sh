f22bdb8cfc76d8e65adcd7aff19c2bc5a88a0238
Derek Richard <drichard@pivotal.io>
2016-12-14 09:56:46 -0800

    Use releng prepared releases zip for fetching closed source releases
    [#135386141]

--- tasks/deploy-pcf.sh
+++ tasks/deploy-pcf.sh
@@ -11,19 +11,19 @@ bosh -n deploy cf-deployment/cf-deployment.yml \
   --ops-file pcf-bosh-ci/ops-files/cf-apps-domain.yml \
   --vars-store new-cf-creds/cf-creds.yml \
   --var system_domain=sys.ol-smokey.gcp.pcf-bosh.cf-app.com \
-  --var "notifications_ui_release_path=file://$(ls "$PWD"/notifications-ui-release/notifications-ui*.tgz)" \
-  --var "notifications_ui_release_version=\"$(cat "$PWD/notifications-ui-release/version")\"" \
-  --var "cf_autoscaling_release_path=file://$(ls "$PWD"/cf-autoscaling-release/cf-autoscaling*.tgz)" \
-  --var "cf_autoscaling_release_version=\"$(cat "$PWD/cf-autoscaling-release/version")\"" \
-  --var "push_apps_manager_release_path=file://$(ls "$PWD"/push-apps-manager-release/*.tgz)" \
-  --var "push_apps_manager_release_version=\"$(cat "$PWD/push-apps-manager-release/version")\"" \
-  --var "cf_release_path=file://$(ls "$PWD"/cf-release/*.tgz)" \
-  --var "cf_release_version=\"$(cat "$PWD/cf-release/version")\"" \
-  --var "postgres_release_path=file://$(ls "$PWD"/postgres-release/*.tgz)" \
-  --var "postgres_release_version=\"$(cat "$PWD/postgres-release/version")\"" \
-  --var "mysql_monitoring_release_path=file://$(ls "$PWD"/mysql-monitoring-release/*.tgz)" \
-  --var "mysql_monitoring_release_version=\"$(cat "$PWD/mysql-monitoring-release/version")\"" \
-  --var "mysql_backup_release_path=file://$(ls "$PWD"/mysql-backup-release/*.tgz)" \
-  --var "mysql_backup_release_version=\"$(cat "$PWD/mysql-backup-release/version")\"" \
-  --var "service_backup_release_path=file://$(ls "$PWD"/service-backup-release/service-backup*.tgz)" \
-  --var "service_backup_release_version=\"$(jq -r .Release.Version "$PWD/service-backup-release/metadata.json")\""
+  --var "cf_release_path=file://$(ls "$PWD"/closed-source-releases/cf-246*.tgz)" \
+  --var "cf_release_version=246.0.2" \
+  --var "postgres_release_path=file://$(ls "$PWD"/closed-source-releases/postgres*.tgz)" \
+  --var "postgres_release_version=8" \
+  --var "notifications_ui_release_path=file://$(ls "$PWD"/closed-source-releases/notifications*.tgz)" \
+  --var "notifications_ui_release_version=31" \
+  --var "cf_autoscaling_release_path=file://$(ls "$PWD"/closed-source-releases/cf-autoscaling*.tgz)" \
+  --var "cf_autoscaling_release_version=67" \
+  --var "push_apps_manager_release_path=file://$(ls "$PWD"/closed-source-releases/push-apps-manager*.tgz)" \
+  --var "push_apps_manager_release_version=659.7" \
+  --var "mysql_monitoring_release_path=file://$(ls "$PWD"/closed-source-releases/mysql-monitoring*.tgz)" \
+  --var "mysql_monitoring_release_version=6" \
+  --var "mysql_backup_release_path=file://$(ls "$PWD"/closed-source-releases/mysql-backup*.tgz)" \
+  --var "mysql_backup_release_version=1.28.0" \
+  --var "service_backup_release_path=file://$(ls "$PWD"/closed-source-releases/service-backup*.tgz)" \
+  --var "service_backup_release_version=17.2.0"
---
+++ tasks/extract-closed-source-releases.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+unzip closed-source-releases-zip/*.zip -d closed-source-releases/
