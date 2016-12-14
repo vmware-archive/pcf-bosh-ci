76f0f925f0fd935ccc9f5dd8714c8498ad23c0f1
Dan Wendorf <dwendorf@pivotal.io>
2016-12-14 11:28:19 -0800

    Use pivotal-defaults ops file
    [finishes #136004729]

--- tasks/deploy-pcf.sh
+++ tasks/deploy-pcf.sh
@@ -8,6 +8,7 @@ bosh -n deploy cf-deployment/cf-deployment.yml \
   --deployment cf \
   --ops-file cf-deployment/opsfiles/gcp.yml \
   --ops-file p-ert/releases.yml \
+  --ops-file p-ert/pivotal-defaults.yml \
   --ops-file pcf-bosh-ci/ops-files/cf-apps-domain.yml \
   --vars-store new-cf-creds/cf-creds.yml \
   --var system_domain=sys.ol-smokey.gcp.pcf-bosh.cf-app.com \
