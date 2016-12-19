aef17ca89120d2d44dcafb089b0ae9858fa4d926
Derek Richard <drichard@pivotal.io>
2016-12-19 10:36:38 -0800

    Update set-pipeline script to reference correct configuration file

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 lpass sync
-fly -t wings sp -p pcf-bosh -c pipeline.yml -l <(lpass show --notes 5986431050471091932)
+fly -t wings sp -p pcf-bosh -c pipelines/pcf-bosh.yml -l <(lpass show --notes 5986431050471091932)
