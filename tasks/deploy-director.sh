1ed3b0a7112590082990755d78fc444fae23da4e
Derek Richard <drichard@pivotal.io>
2016-12-22 11:10:28 -0800

    Generate bosh-vars
    [#136066129]
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- tasks/deploy-director.sh
+++ tasks/deploy-director.sh
@@ -2,6 +2,23 @@
 
 set -e
 
+cat <<BOSHVARS > bosh-vars.yml
+---
+director_name: pcf-bosh-director
+internal_cidr: $(jq -r .cf_cidr terraform-state/metadata)
+internal_gw: $(jq -r .cf_gateway terraform-state/metadata)
+internal_ip: $(jq -r .cf_director_internal_ip terraform-state/metadata)
+external_ip: $(jq -r .director_external_ip terraform-state/metadata)
+network: $(jq -r .network_name terraform-state/metadata)
+project_id: $(jq -r .project terraform-state/metadata)
+subnetwork: $(jq -r .cf_subnet terraform-state/metadata)
+zone: $(jq -r .azs[0] terraform-state/metadata)
+gcp_credentials_json: |
+    $(echo $GOOGLE_JSON_KEY)
+credhub_aes_key: 547378EB7909D6078C14FCBE631D3BAF
+file_path_to_credhub_release: "file://$PWD/credhub-release/credhub-0.3.0.tgz"
+BOSHVARS
+
 cp bosh-state/bosh-state.json new-bosh-state/bosh-state.json
 cp bosh-creds/bosh-creds.yml new-bosh-creds/bosh-creds.yml
 
@@ -14,7 +31,4 @@ bosh create-env bosh-deployment/bosh.yml \
   --ops-file pcf-bosh-ci/ops-files/credhub.yml \
   --ops-file pcf-bosh-ci/ops-files/director-overrides.yml \
   --vars-store new-bosh-creds/bosh-creds.yml \
-  --vars-file bosh-vars/bosh-vars.yml \
-  --var file_path_to_credhub_release="file://$PWD/credhub-release/credhub-0.3.0.tgz" \
-  --var gcp_credentials_json="$GOOGLE_JSON_KEY" \
-  --var external_ip="$(jq -r .director_external_ip terraform-state/metadata)"
+  --vars-file bosh-vars.yml
