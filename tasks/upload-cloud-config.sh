373cbc15eadc0baa995f7893b40f8b22e3193395
Derek Richard <drichard@pivotal.io>
2016-12-22 12:21:40 -0800

    Parameterize environment-specific cloud-config details
    [#136066129]
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- tasks/upload-cloud-config.sh
+++ tasks/upload-cloud-config.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+set -e
+
 source pcf-bosh-ci/scripts/load-director-environment.sh bosh-creds/bosh-creds.yml
 
-bosh -n update-cloud-config cloud-config/cloud-config.yml
+cat <<BOSHVARS > bosh-vars.yml
+---
+internal_cidr: $(jq -r .cf_cidr terraform-state/metadata)
+internal_gw: $(jq -r .cf_gateway terraform-state/metadata)
+network: $(jq -r .network_name terraform-state/metadata)
+subnetwork: $(jq -r .cf_subnet terraform-state/metadata)
+vm_tag: $(jq -r .cf_internal_tags terraform-state/metadata)
+http_backend_service: $(jq -r .http_lb_backend_name terraform-state/metadata)
+ws_target_pool: $(jq -r .ws_router_pool terraform-state/metadata)
+public_tags: $(jq -r .cf_public_tags terraform-state/metadata)
+tcp_target_pool: $(jq -r .tcp_router_pool terraform-state/metadata)
+tcp_tags: $(jq -r .cf_tcp_tags terraform-state/metadata)
+ssh_target_pool: $(jq -r .ssh_router_pool terraform-state/metadata)
+ssh_tags: $(jq -r .cf_ssh_tags terraform-state/metadata)
+z1: $(jq -r .azs[0] terraform-state/metadata)
+z2: $(jq -r .azs[1] terraform-state/metadata)
+z3: $(jq -r .azs[2] terraform-state/metadata)
+reserved_ips: $(jq -r .cf_reserved_ips terraform-state/metadata)
+static_ip_start: $(jq -r .cf_static_ip_start terraform-state/metadata)
+static_ip_end: $(jq -r .cf_static_ip_end terraform-state/metadata)
+BOSHVARS
+
+bosh -n update-cloud-config cloud-config/cloud-config.yml --vars-file bosh-vars.yml
