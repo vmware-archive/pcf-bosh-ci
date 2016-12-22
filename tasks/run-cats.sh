20f9fdafea8a76ebb9c437727f17499a140e2bc1
Dan Wendorf <dwendorf@pivotal.io>
2016-12-22 15:32:30 -0800

    Dynamically generate cats config json
    - Use cf-creds and terraform state
    [#136066129]
    
    Signed-off-by: Derek Richard <drichard@pivotal.io>

--- tasks/run-cats.sh
+++ tasks/run-cats.sh
@@ -2,7 +2,29 @@
 
 set -e
 
-export CONFIG="$PWD/cf-cats-config/cf-cats-config.json"
+cf_creds_json=$(ruby -ryaml -rjson -e "puts JSON.pretty_generate(YAML.load_file('cf-creds/cf-creds.yml'))")
+
+cat <<CFCATSCONFIG > cf-cats-config.json
+{
+  "addresses": ["$(jq -r .tcp_domain terraform-state/metadata)"],
+  "api": "api.$(jq -r .sys_domain terraform-state/metadata)",
+  "admin_user": "admin",
+  "admin_password": "$(echo "$cf_creds_json" | jq -r .uaa_scim_users_admin_password)",
+  "skip_ssl_validation": true,
+  "use_http":true,
+  "apps_domain": "$(jq -r .apps_domain terraform-state/metadata)",
+  "include_http_routes": true,
+  "oauth": {
+    "token_endpoint": "https://uaa.$(jq -r .sys_domain terraform-state/metadata)",
+    "client_name": "tcp_emitter",
+    "client_secret": "$(echo "$cf_creds_json" | jq -r .uaa_clients_tcp_emitter_secret)",
+    "port": 443,
+    "skip_ssl_validation": true
+  }
+}
+CFCATSCONFIG
+
+export CONFIG="$PWD/cf-cats-config.json"
 
 export CF_GOPATH="/go/src/github.com/cloudfoundry/"
 
