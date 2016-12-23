94d9aea6e7984518068552271a728ac08145df9b
Dan Wendorf <dwendorf@pivotal.io>
2016-12-22 17:45:15 -0800

    Template rats-config
    [#136066129]

--- tasks/run-rats.sh
+++ tasks/run-rats.sh
@@ -2,7 +2,27 @@
 
 set -e
 
-export CONFIG="$PWD/cf-rats-config/cf-rats-config.json"
+cat <<CFRATSCONFIG > cf-rats-config.json
+{
+  "addresses": ["$(jq -r .tcp_domain terraform-state/metadata)"],
+  "api": "api.$(jq -r .sys_domain terraform-state/metadata)",
+  "admin_user": "admin",
+  "admin_password": "$(echo "$cf_creds_json" | jq -r .uaa_scim_users_admin_password)",
+  "skip_ssl_validation": true,
+  "use_http":true,
+  "apps_domain": "$(jq -r .apps_domain terraform-state/metadata)",
+  "include_http_routes": true,
+  "oauth": {
+    "token_endpoint": "https://uaa.$(jq -r .sys_domain terraform-state/metadata)",
+    "client_name": "tcp_emitter",
+    "client_secret": "$(echo "$cf_creds_json" | jq -r .uaa_clients_tcp_emitter_secret)",
+    "port": 443,
+    "skip_ssl_validation": true
+  }
+}
+CFRATSCONFIG
+
+export CONFIG="$PWD/cf-rats-config.json"
 export GOPATH="$PWD/routing-release"
 export PATH="${GOPATH}/bin":$PATH
 
