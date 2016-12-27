5a30da420787e0ef95cd288c870d81126578840b
Dan Wendorf <dwendorf@pivotal.io>
2016-12-27 15:25:00 -0800

    Add task for running push-apps-manager errand
    Make sure pcf-deployment and errands cannot run simultaneously with bosh-deployment serial_group
    
    [#135459691]

---
+++ tasks/push-apps-manager.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+eval "$(pcf-bosh-ci/scripts/director-environment bosh-creds/*-bosh-creds.yml terraform-state/metadata)"
+
+bosh -n -d cf run-errand push-apps-manager
