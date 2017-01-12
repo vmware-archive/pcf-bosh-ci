0fc738e483937ad1b7f79c7685ae0f48d367a26f
Derek Richard <drichard@pivotal.io>
2017-01-11 16:50:30 -0800

    Add upload release task for mysql-monitoring release upload
    [#137405997]
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

---
+++ tasks/upload-release.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+eval "$(pcf-bosh-ci/scripts/director-environment bosh-vars-store/*-bosh-vars-store.yml terraform-state/metadata)"
+
+bosh -n upload-release "release/*.tgz"
