f2c4f149f1f0faed261d2b30d36b8a060f744b68
Derek Richard <drichard@pivotal.io>
2017-01-12 08:32:15 -0800

    Remove quoting of upload-release argument
    - Bash will not expand the glob in double quotes
    
    [#137405997]

--- tasks/upload-release.sh
+++ tasks/upload-release.sh
@@ -4,4 +4,4 @@ set -e
 
 eval "$(pcf-bosh-ci/scripts/director-environment bosh-vars-store/*-bosh-vars-store.yml terraform-state/metadata)"
 
-bosh -n upload-release "release/*.tgz"
+bosh -n upload-release release/*.tgz
