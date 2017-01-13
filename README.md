68967cc7a4f5c1aad5136b826fedd3dfed340df3
Derek Richard <drichard@pivotal.io>
2017-01-13 14:41:45 -0800

    Do not deploy credhub to bosh directors
    The project is not currently in a state that lets us use it with BOSH. We can
    bring it back when it is ready. See #135152951 for the followup.
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- README.md
+++ README.md
@@ -4,9 +4,7 @@ PCF BOSH CI is a collection of tasks and ops-files to help deploy and manage PCF
 
 ## BOSH
 
-BOSH is deployed using [https://github.com/cloudfoundry/bosh-deployment](bosh-deployment). The vars necessary to deploy with [tasks/deploy-director.sh](deploy-director.sh) are the same as bosh-deployment with one addition:
-
-- **credhub_aes_key**: A 128-bit hex-encoded AES key (e.g. D673ACD01DA091B08144FBC8C0B5F524)
+BOSH is deployed using [https://github.com/cloudfoundry/bosh-deployment](bosh-deployment). The vars necessary to deploy with [tasks/deploy-director.sh](deploy-director.sh) are the same as bosh-deployment:
 
 ## Cloud Foundry
 
--- tasks/deploy-director.sh
+++ tasks/deploy-director.sh
@@ -17,8 +17,6 @@ bosh_director_tags: $(jq -r .bosh_director_tags terraform-state/metadata)
 bosh_director_domain: $(jq -r .bosh_director_domain terraform-state/metadata)
 gcp_credentials_json: |
     $(echo $GOOGLE_JSON_KEY)
-credhub_aes_key: 547378EB7909D6078C14FCBE631D3BAF
-file_path_to_credhub_release: "file://$PWD/credhub-release/credhub-0.3.0.tgz"
 BOSHVARS
 
 cp bosh-state/*-bosh-state.json new-bosh-state/bosh-state.json
@@ -30,7 +28,6 @@ bosh create-env bosh-deployment/bosh.yml \
   --ops-file bosh-deployment/gcp/cpi.yml \
   --ops-file bosh-deployment/uaa.yml \
   --ops-file pcf-bosh-ci/ops-files/uaa-with-external-domain.yml \
-  --ops-file pcf-bosh-ci/ops-files/credhub.yml \
   --ops-file pcf-bosh-ci/ops-files/director-overrides.yml \
   --vars-store new-bosh-vars-store/bosh-vars-store.yml \
   --vars-file bosh-vars.yml
