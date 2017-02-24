08f501eb6edb58ceaa6228068df68e05332b9d87
Dan Wendorf <dwendorf@pivotal.io>
2017-02-24 15:06:35 -0800

    Add delete-director tasks

---
+++ tasks/delete-director-aws.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+
+internal_cidr=$(jq -r .public_subnet_cidrs[0] terraform-state/metadata)
+internal_gw=$(prips "$internal_cidr" | awk 'NR==2 {print $0}')
+internal_ip=$(prips "$internal_cidr" | awk 'NR==5 {print $0}')
+
+chmod 600 bosh-private-key/bosh-private-key
+
+cat <<BOSHVARS > bosh-vars.yml
+---
+director_name: pcf-bosh-director
+az: $(jq -r .azs[0] terraform-state/metadata)
+subnet_id: $(jq -r .public_subnet_ids[0] terraform-state/metadata)
+internal_cidr: $internal_cidr
+internal_gw: $internal_gw
+internal_ip: $internal_ip
+external_ip: $(jq -r .bosh_director_external_ip terraform-state/metadata)
+access_key_id: $(jq -r .bosh_iam_user_access_key terraform-state/metadata)
+secret_access_key: $(jq -r .bosh_iam_user_secret_access_key terraform-state/metadata)
+default_key_name: $(jq -r .default_key_name terraform-state/metadata)
+default_security_groups: [$(jq -r .vms_security_group_id terraform-state/metadata)]
+bosh_director_security_groups: [$(jq -r .bosh_security_group_id terraform-state/metadata)]
+region: $(jq -r .region terraform-state/metadata)
+bosh_director_domain: $(jq -r .bosh_director_domain terraform-state/metadata)
+bosh_director_tags: director
+BOSHVARS
+
+cp bosh-state/*-bosh-state.json new-bosh-state/bosh-state.json
+cp bosh-vars-store/*-bosh-vars-store.yml new-bosh-vars-store/bosh-vars-store.yml
+
+bosh delete-env bosh-deployment/bosh.yml \
+  --state new-bosh-state/bosh-state.json \
+  --ops-file bosh-deployment/external-ip-not-recommended.yml \
+  --ops-file bosh-deployment/aws/cpi.yml \
+  --ops-file bosh-deployment/uaa.yml \
+  --ops-file pcf-bosh-ci/ops-files/uaa-with-external-domain.yml \
+  --ops-file pcf-bosh-ci/ops-files/director-overrides.yml \
+  --ops-file pcf-bosh-ci/ops-files/aws-director-overrides.yml \
+  --vars-store new-bosh-vars-store/bosh-vars-store.yml \
+  --vars-file bosh-vars.yml \
+  --var-file private_key=bosh-private-key/bosh-private-key
---
+++ tasks/delete-director.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+cat <<BOSHVARS > bosh-vars.yml
+---
+director_name: pcf-bosh-director
+internal_cidr: $(jq -r .cf_cidr terraform-state/metadata)
+internal_gw: $(jq -r .cf_gateway terraform-state/metadata)
+internal_ip: $(jq -r .cf_director_internal_ip terraform-state/metadata)
+external_ip: $(jq -r .director_external_ip terraform-state/metadata)
+network: $(jq -r .network_name terraform-state/metadata)
+project_id: $(jq -r .project terraform-state/metadata)
+subnetwork: $(jq -r .cf_subnet terraform-state/metadata)
+zone: $(jq -r .azs[0] terraform-state/metadata)
+bosh_director_tags: $(jq -r .bosh_director_tags terraform-state/metadata)
+bosh_director_domain: $(jq -r .bosh_director_domain terraform-state/metadata)
+gcp_credentials_json: |
+    $(echo $GOOGLE_JSON_KEY)
+BOSHVARS
+
+cp bosh-state/*-bosh-state.json new-bosh-state/bosh-state.json
+cp bosh-vars-store/*-bosh-vars-store.yml new-bosh-vars-store/bosh-vars-store.yml
+
+bosh delete-env bosh-deployment/bosh.yml \
+  --state new-bosh-state/bosh-state.json \
+  --ops-file bosh-deployment/external-ip-not-recommended.yml \
+  --ops-file bosh-deployment/gcp/cpi.yml \
+  --ops-file bosh-deployment/uaa.yml \
+  --ops-file pcf-bosh-ci/ops-files/uaa-with-external-domain.yml \
+  --ops-file pcf-bosh-ci/ops-files/director-overrides.yml \
+  --vars-store new-bosh-vars-store/bosh-vars-store.yml \
+  --vars-file bosh-vars.yml
